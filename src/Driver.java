ec4238903c7cb5c96fa8a39bb795e8c0a4ab67db
Renish Madhukar <renishdmadhukar@gmail.com>
2023-07-27 22:52:10 -0400

    Added new problem

--- src/Driver.java
+++ src/Driver.java
@@ -470,7 +470,7 @@ public class Driver {
     }
 
     public static void trappingRainWater() {
-
+        System.out.println("trappingRainWater::max: " + TrappingRainWater.trap(new int[] {0,1,0,2,1,0,1,3,2,1,2,1}));
     }
 
     public static BinaryTree binaryTreeBuilder() {
--- src/Problems/TrappingRainWater.java
+++ src/Problems/TrappingRainWater.java
@@ -1,4 +1,34 @@
 package Problems;
 
 public class TrappingRainWater {
+    public static int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+
+        int left = 0;
+        int right = height.length - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int totalWater = 0;
+
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                if (height[left] > leftMax) {
+                    leftMax = height[left];
+                }
+                int water = leftMax - height[left];
+                totalWater += water;
+                left++;
+            } else {
+                if (height[right] > rightMax) {
+                    rightMax = height[right];
+                }
+                int water = rightMax - height[right];
+                totalWater += water;
+                right--;
+            }
+        }
+        return totalWater;
+    }
 }
