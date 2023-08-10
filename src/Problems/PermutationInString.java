34c58f118b921f0dd12eede912d9d21fb9ad328e
Renish Madhukar <renishdmadhukar@gmail.com>
2023-08-09 23:43:16 -0400

    Added problem notes

--- src/Problems/PermutationInString.java
+++ src/Problems/PermutationInString.java
@@ -3,6 +3,8 @@ package Problems;
 import java.util.Arrays;
 
 public class PermutationInString {
+    // Algorithm: find if frequency arrays become same at any point while iterating over two strings.
+    // for that, always need to maintain the window of length of string S1.
     public static boolean checkInclusion(String s1, String s2) {
         int n = s1.length();
         int m = s2.length();
@@ -15,10 +17,12 @@ public class PermutationInString {
         int[] freqS2 = new int[26];
         for (int j = 0; j < m; j++) {
             freqS2[s2.charAt(j) - 'a']++;
-            // sliding window has moved past the length of s1
+            // sliding window size is greater than the length of s1
             if (j >= n) {
+                // shrink window from left end and remove frequency from array for left element
                 freqS2[s2.charAt(j - n) - 'a']--;
             }
+            // checks if there are 26 matches i.e. if frequency arrays are same.
             if (Arrays.equals(freqS1, freqS2)) {
                 return true;
             }
