0962716590e280cfa0f6e8fee46c01f366719eb1
Renish Madhukar <renishdmadhukar@gmail.com>
2023-08-11 23:38:18 -0400

    Added new problem

--- src/Problems/MinimumWindowSubstring.java
+++ src/Problems/MinimumWindowSubstring.java
@@ -1,6 +1,4 @@
 package Problems;
-import java.util.HashMap;
-import java.util.Map;
 
 public class MinimumWindowSubstring {
     public static String minWindow(String s, String t) {
@@ -12,18 +10,50 @@ public class MinimumWindowSubstring {
             return "";
         }
 
-        Map<Character, Integer> mapT = new HashMap<>();
+        // Assuming ASCII character set
+        int[] targetFreq = new int[128];
+        int[] windowFreq = new int[128];
 
-        for (int i = 0; i < t.length(); i++) {
-            int count = mapT.getOrDefault(t.charAt(i), 0);
-            mapT.put(t.charAt(i), count + 1);
+        // Count the frequency of characters in string t
+        for (char c : t.toCharArray()) {
+            targetFreq[c]++;
         }
 
-        int need = mapT.size();
-        int have = 0;
-        int left = 0, right = 0;
-        Map<Character, Integer> windowCounts = new HashMap<>();
+        int left = 0; // Left pointer of the sliding window
+        int minLen = Integer.MAX_VALUE; // Minimum window length found so far
+        int minStart = 0; // Starting index of the minimum window substring
+        int matched = 0; // Count of characters from string t that are currently in the window
 
-        return "IN PROGRESS";
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+
+            if (targetFreq[c] > 0) {
+                windowFreq[c]++;
+                if (windowFreq[c] <= targetFreq[c]) {
+                    matched++;
+                }
+            }
+
+            while (matched == t.length()) {
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    minStart = left;
+                }
+
+                char leftChar = s.charAt(left);
+
+                if (targetFreq[leftChar] > 0) {
+                    windowFreq[leftChar]--;
+                    if (windowFreq[leftChar] < targetFreq[leftChar]) {
+                        matched--;
+                    }
+                }
+                left++;
+            }
+        }
+        if (minLen == Integer.MAX_VALUE) {
+            return "";
+        }
+        return s.substring(minStart, minStart + minLen);
     }
 }
