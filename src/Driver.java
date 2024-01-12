bfdd8b18dfcecd8692d1dad7bad9313dc9b610ee
renishmadhukar <renishdmadhukar@gmail.com>
2024-01-11 21:53:30 -0500

    Updated printMatrix method

--- src/Driver.java
+++ src/Driver.java
@@ -814,9 +814,9 @@ public class Driver {
     }
 
     public static void printMatrix(int[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
+        for (int[] integers : matrix) {
             for (int j = 0; j < matrix[0].length; j++) {
-                System.out.print(String.format("%2d ", matrix[i][j]));
+                System.out.printf("%2d ", integers[j]);
             }
             System.out.println();
         }
